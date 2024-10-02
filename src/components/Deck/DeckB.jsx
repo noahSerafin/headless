59fded5ed6c4ce748e81e29b1693266e873b33f4
noahSerafin <noah.serafin04@gmail.com>
2024-10-02 22:37:17 +0100

    spinning

--- src/components/Deck/DeckB.jsx
+++ src/components/Deck/DeckB.jsx
@@ -13,10 +13,13 @@ const Deck = (audio) => {
   const [isPlaying, setIsPlaying] = useState(false)
   const currentTimeRef = useRef(0)
   const [displayTime, setDisplayTime] = useState(currentTimeRef.current)
+  const recordRef = useRef(null); 
   const [isLoaded, setIsLoaded] = useState(false)
   const [angle, setAngle] = useState(0)
   const [active, setActive] = useState(false)//useState?
   const [style, setStyle] = useState({transform: 'rotate(0deg)'})
+  const [startTime, setStartTime] = useState(0)
+  const playerRef = useRef(null);
 
   const handleDrop = (e) => {
     e.preventDefault();
@@ -24,7 +27,7 @@ const Deck = (audio) => {
     if(trackData) {
       const track = JSON.parse(trackData);
       setCurrentTrack(track)
-      console.log(currentTrack)
+      console.log('currentTrack:', currentTrack)
     }
   }
 
@@ -33,91 +36,83 @@ const Deck = (audio) => {
   };
 
   const calculateElapsedTime = () => {
-    return currentTimeRef.current + (Tone.now() - startTime); // Actual time passed
+    return (Tone.now() - startTime); // Actual time passed
+    //return currentTimeRef.current + (Tone.now() - startTime); // Actual time passed
   };
   
   //audio methods
-  const [startTime, setStartTime] = useState(0)
-  const playerRef = useRef(null);
-
-    const dummyTrack = {
-        "__typename": "Song",
-        "id": "cG9zdDoxOA==",
-        "title": "Drone",
-        "content": "<p>Visual noise generation</p>\n",
-        "songMeta": {
-          "__typename": "SongMeta",
-          "artist": "Seraf",
-          "coverArt": {
-            "__typename": "AcfMediaItemConnectionEdge",
-            "node": {
-              "__typename": "MediaItem",
-              "sourceUrl": "http://localhost/wp-headless/server/wp-content/uploads/2024/09/Screenshot-2024-04-30-202244.png"
-            }
-          },
-          "file": {
-            "__typename": "AcfMediaItemConnectionEdge",
-            "node": {
-              "__typename": "MediaItem",
-              "mediaItemUrl": "http://localhost/wp-headless/server/wp-content/uploads/2024/09/drone.mp3"
-            }
-          },
-          "bpm": 130
-        }
-    }
-    
-    const trackurl = dummyTrack.songMeta.file.node.mediaItemUrl
-    //console.log('track:', trackurl)
       
-    useEffect(() => {
-      if(currentTrack){
-        playerRef.current = new Tone.Player({
-          url: currentTrack.songMeta.file.node.mediaItemUrl,
-          autostart: false,
-        }).toDestination();
-        console.log('player:', playerRef.current.state)
-      } //playerRef.current.volume.value = value;
+  useEffect(() => {
+
+    if(currentTrack){
+      //console.log('currentUrl:', currentTrack.songMeta.file.node.mediaItemUrl)
+      if (playerRef.current){
+        playerRef.current.dispose();
+      }
+      currentTimeRef.current = 0
+      setDisplayTime(0)
+      setIsLoaded(false)
+      setIsPlaying(false)
+
+      playerRef.current = new Tone.Player({
+        url: currentTrack.songMeta.file.node.mediaItemUrl,
+        autostart: false,
+        onload: () => {
+          console.log('Track loaded');
+          setIsLoaded(true); // Set isLoaded to true once the track is fully loaded
+        },
+        onerror: (error) => {
+          console.error('Error loading track:', error);
+          setIsLoaded(false);
+        }
+      }).toDestination();
+      console.log('player:', playerRef.current.state)
+    } //playerRef.current.volume.value = value;
     
-        return () => {
-          // Clean up the player instance when the component unmounts
-          if(playerRef.current){
-            playerRef.current.dispose();
-          }
-        };
-    }, [currentTrack]);//url, value
+      return () => {
+        // Clean up the player instance when the component unmounts
+        if(playerRef.current){
+        playerRef.current.dispose();
+      }
+    };
+  }, [currentTrack]);//url, value
 
     const playPause = () => {
+      if(playerRef.current.loaded){
         setIsPlaying((prevIsPlaying) => {
+          console.log(playerRef.current.loaded)
           const player = playerRef.current
           if (prevIsPlaying) {
             const elapsedTime = calculateElapsedTime()
-            currentTimeRef.current = elapsedTime;
-            player.stop();
+            currentTimeRef.current += elapsedTime;
+            playerRef.current.stop();
           } else {
-            player.start(0, currentTimeRef.current);
+            playerRef.current.start(0, currentTimeRef.current);
             setStartTime(Tone.now());
           }
           console.log(player.state, ' at ', currentTimeRef.current)
-    
+      
           return !prevIsPlaying;
         });
+      }
     };
     
     const Cue = () => {
         const player = playerRef.current
         console.log('cue at ', currentTimeRef.current)   
         if(isPlaying){ //playing.state == stopped/started
-          currentTimeRef.current = Tone.now();
+          setStartTime(Tone.now());
         } else {
-          player.start(0, currentTimeRef.current)
+          player.start(0, startTime)
           setIsPlaying(true)
         }
     }
     
     const restart = () => {
         const player = playerRef.current
-        setStartTime(Tone.now());
         currentTimeRef.current = 0
+        setStartTime(Tone.now());
+        setDisplayTime(0)
         setIsPlaying(true)
         player.start(0)
         //reset angle
@@ -126,11 +121,17 @@ const Deck = (audio) => {
   // Tick function to update display time
   useEffect(() => {
     let interval;
+    console.log('record:', recordRef)
     if (isPlaying) {
       interval = setInterval(() => {
         // Update currentTimeRef while playing
-        currentTimeRef.current += 0.1; // Increment time for display (100 ms)
-        setDisplayTime(currentTimeRef.current)
+        //currentTimeRef.current += 0.1; // Increment time for display (100 ms)
+        const elapsedTime = currentTimeRef.current + calculateElapsedTime()
+        setDisplayTime(elapsedTime)
+        if(recordRef.current){
+          const rotation = (elapsedTime)
+          recordRef.current.style.transform = `rotate(${rotation*2}rad)`;
+        }
       }, 100); // Update every 100 ms
     }
     return () => {
@@ -174,11 +175,13 @@ const Deck = (audio) => {
         x: parseFloat(element.dataset.centerX) || 0,
         y: parseFloat(element.dataset.centerY) || 0,
       };
-      var angle = getDragAngle(event);
-      console.log(angle)
+
+      var newAngle = getDragAngle(event);
+      console.log(newAngle)
       
       // update transform style on dragmove
-      element.style.transform = 'rotate(' + angle + 'rad' + ')';
+      //setStyle({{transform: rotate(' + angle + 'rad' + ')}};
+      element.style.transform = 'rotate(' + newAngle + 'rad' + ')';
     }
   }
   const stopRotate = function (event) {
@@ -194,14 +197,14 @@ const Deck = (audio) => {
     <div className="deck-container" onDrop={handleDrop} onDragOver={handleDragOver}>
       <div className='deck'> 
         <div id='container'> 
-          <div id="rotate" onMouseDown={(e) => startRotate(e)} onMouseMove={(e) => rotate(e)} onMouseUp={(e) => stopRotate(e)} onMouseLeave={(e) => stopRotate(e)} style={style} className={`active-${active}`}>
+          <div ref={recordRef} id="rotate" onMouseDown={(e) => startRotate(e)} onMouseMove={(e) => rotate(e)} onMouseUp={(e) => stopRotate(e)} onMouseLeave={(e) => stopRotate(e)} className={`active-${active}`}>
             <div id="drag"></div>
           </div>
         </div>
       </div>
       {currentTrack ? (
-      <div className="audioplayer">
-        <div className="flex">
+      <div className='audioplayer'>
+        <div className={"flex playerbuttons-"+isLoaded}>
             <button className="playbtn" onClick={() => Cue()}>Cue</button>
             <button className="playbtn" onClick={playPause}>{isPlaying ? 'Pause' : 'Play'}</button>
             <button className="playbtn" onClick={() => restart()}>Restart</button>
